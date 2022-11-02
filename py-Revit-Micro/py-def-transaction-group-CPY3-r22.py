78fe552ff2ee5a5fb5e33ba4b4be01a14b53c009
Ron <apsis0215@gmail.com>
2022-11-02 12:22:40 -0600

    Update py-def-transaction-group-CPY3-r22.py

--- py-Revit-Micro/py-def-transaction-group-CPY3-r22.py
+++ py-Revit-Micro/py-def-transaction-group-CPY3-r22.py
@@ -1,8 +1,8 @@
 from Autodesk.Revit.DB import Transaction, TransactionGroup ##For outside and inner transactions
 ########################################
-trans=TransactionGroup(doc, 'trans:DynamoSetDraftCrop') ##Transaction group start name
+trans=TransactionGroup(doc, 'DYN:[some name for transaction]') ##Transaction group start name 'DYN:' optional
 trans.Start()                                       ##Start outside transaction
-    transGrp=Transaction(doc, **Trans_Name_Str**)   ##GROUP Transaction(s)
+    transGrp=Transaction(doc, 'DYN:TransGrpName')   ##GROUP Transaction(s) 'DYN:' optional
     transGrp.Start()                                ##Start Group transaction
     ##Do a wrapped thing
     transGrp.Commit()                               ##Commit group transaction for current view : 
